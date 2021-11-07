9c84c77c808071bf5c768fcf86874cf411d8269e
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-11-07 17:58:14 -0500

    Finished Ch7

--- .history/app/controllers/users_controller_20211107175230.rb
+++ .history/app/controllers/users_controller_20211107175230.rb
@@ -10,9 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-        else
+      redirect_to @user    else
       render 'new'
     end
   end
